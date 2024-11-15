41e699fde5321ecf61a75b07ce1d6a504497a932
anahayma <anaavillo8@gmail.com>
2024-11-15 10:17:40 -0400

    Add files via upload

---
+++ js/datosM.js
@@ -0,0 +1,15 @@
+function aaa(){
+	var c=document.getElementById('nn').value;
+	var d=document.getElementById('n2').value;
+
+	if (c=="ana"){
+		if (d=="123456"){
+			alert ('bienvenido');
+			window.location.href='opciones.html';
+		}
+		else{
+			alert('error de datos');
+		}
+	}
+
+}
